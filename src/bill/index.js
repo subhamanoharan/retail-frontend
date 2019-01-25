449ae3307e860929d2e5d03d48486c32d6a7085e
Subha Manoharan <subhamanoharanbe@gmail.com>
2019-01-25 15:29:00 +0530

    Subha | Update quantity when bar code is scanned again

--- src/bill/index.js
+++ src/bill/index.js
@@ -1,4 +1,5 @@
 import React, { Component} from "react";
+import uuid from 'uuid/v4';
 
 import AddNewItem from '../addNewItem';
 import ItemsTable from '../itemsTable';
@@ -8,25 +9,43 @@ export default class Bill extends Component {
   constructor(props) {
     super(props);
     this.state = { items: [
-      {name: 'dummy1', mrp: 30, sp: 20, quantity: 1},
-      {name: 'dummy2', mrp: 30, sp: 20, quantity: 1}
+      {code: 'dummy1', name: 'dummy1', mrp: 30, sp: 20, quantity: 1},
+      {code: 'dummy2', name: 'dummy2', mrp: 30, sp: 20, quantity: 1}
     ] };
-    this.onAddNewItem = this.onAddNewItem.bind(this);
+    this.onManuallyItemAdded = this.onManuallyItemAdded.bind(this);
     this.onBarCodeScanned = this.onBarCodeScanned.bind(this);
   }
 
-  onAddNewItem(item){
-    this.setState({items: [...this.state.items, item]});
+  _onAddItem(item){
+    this.setState({items: [...this.state.items, {...item}]});
+  }
+
+  _onUpdateItem(itemToEdit){
+    const updatedItems = this.state.items
+      .map((item) => ({...(item.code === itemToEdit.code) ? itemToEdit : item}));
+    this.setState({items: updatedItems});
+  }
+
+  _findItem(code) {
+      return this.state.items.find(({code: itemCode}) => itemCode === code);
   }
 
   onBarCodeScanned(code){
-    this.onAddNewItem({name: code, mrp: 31, sp: 21, quantity: 12});
+    const foundItem = this._findItem(code);
+    if(foundItem)
+      this._onUpdateItem({...foundItem, quantity: 12 + foundItem.quantity})
+    else
+      this._onAddItem({name: code, mrp: 31, sp: 21, quantity: 12, code});
+  }
+
+  onManuallyItemAdded(item){
+    this._onAddItem({...item, code: uuid()});
   }
 
   render() {
     return (
       <div onMouseDown={(e) => e.preventDefault() }>
-        <AddNewItem onAdd={this.onAddNewItem} />
+        <AddNewItem onAdd={this.onManuallyItemAdded} />
         <ItemsTable items={this.state.items} />
         <BarCodeInputField onScanComplete={this.onBarCodeScanned}/>
       </div>
